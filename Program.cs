dd02ee50bcbc480224bc10c4446ec1133d576993
Matheus-Boff <matheus.boff002@edu.pucrs.br>
2025-08-11 20:33:25 -0300

    commit 2

--- Program.cs
+++ Program.cs
@@ -13,7 +13,11 @@ namespace ExerciciosRecursao
             // System.Console.WriteLine(somaAte0(5));
             // System.Console.WriteLine(somaJAteK(5, 9));
             // System.Console.WriteLine(isPal("arara"));
-            // System.Console.WriteLine(convBase2(3));
+            // System.Console.WriteLine(convBase2(16));
+            // System.Console.WriteLine(somaArray(new List<int>{5,6,7,8,9,10}));
+            // System.Console.WriteLine(findBiggest(new List<int>{1,4,5,6,9}));
+            // System.Console.WriteLine(findSubStr("abacaxi", "cax"));
+            // System.Console.WriteLine(nroDigit(333));
 
         }
 
@@ -68,16 +72,55 @@ namespace ExerciciosRecursao
         {
             if (n == 1) return "1";
 
-            if (n % 2 == 0) return "0" + convBase2(n / 2);
+            if (n % 2 == 0) return convBase2(n / 2) + "0";
 
-            return "1" + convBase2(n / 2);
+            return convBase2(n / 2) + "1";
         }
 
-        // public static int somaArray(List<int> ints)
-        // {
-        //     if (ints.Count == 0) return 0;
+        public static int somaArray(List<int> ints)
+        {
+            if (ints.Count == 1) return ints[0];
+            ints[0] = ints[0] + ints[1];
+            ints.RemoveAt(1);
+            return 0 + somaArray(ints);
+        }
 
-        // }
+        public static int findBiggest(List<int> array)
+        {
+            if (array.Count == 1)
+            {
+                return array[0];
+            }
+
+            if (array[1] > array[0])
+            {
+                array[0] = array[1];
+                array.RemoveAt(1);
+                return 0 + findBiggest(array);
+            }
+            else
+            {
+                array.RemoveAt(1);
+                return 0 + findBiggest(array);
+            }
+        }
+
+        public static bool findSubStr(string str, string match)
+        {
+            if (str.Length < match.Length) return false;
+
+            if (str.StartsWith(match)) return true;
+
+            return findSubStr(str.Substring(1), match);
+        }
+
+        public static int nroDigit(int n)
+        {
+            if (n < 10 && n > 0) return 1;
+
+            return 1 + nroDigit(n / 10);
+        }
+
         
     }
 }
